5cc4c8d65055c02ca75256a78888ab520ebf2c94
ddnnss <ddnnss.i1@gmail.com>
2019-06-02 16:08:43 +0300

    первый вариант, без адаптива

--- app/js/main.js
+++ app/js/main.js
@@ -4,21 +4,37 @@ console.log("ready!");
 let modal = document.getElementById('myModal');
 
 let btn = document.getElementById("header-order-btn");
+let btn1 = document.getElementById("footer-order-btn");
 
 let span = document.getElementsByClassName("close")[0];
+let mobile_button = document.getElementById("mobile-button");
+let mobile_menu = document.getElementById("menu-mobile");
 let divs = document.getElementsByClassName('how-it-works-circle')
 btn.onclick = function () {
     modal.style.display = "block";
 }
-
+btn1.onclick = function () {
+    modal.style.display = "block";
+}
 span.onclick = function () {
     modal.style.display = "none";
+
+}
+
+mobile_button.onclick = function () {
+      // console.log(this)
+    // mobile_menu.style.display = "block";
+      mobile_menu.classList.toggle('is-visible');
+
 }
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+
+
 for (let div in divs) {
     divs[div].addEventListener('mouseover',function(){
         let circle = this.getElementsByClassName('circle-grey')[0]
@@ -60,3 +76,41 @@ for (let div in divs) {
 }
 
 
+function homeCallback(e) {
+    e.preventDefault();
+    client_name = document.getElementById("home-client-name").value;
+    client_phone = document.getElementById("home-client-phone").value;
+    console.log('send form');
+        $.ajax({
+            type:"POST",
+            url:'/user/callback/',
+            data:{
+                'client_name':client_name,
+                'client_phone':client_phone
+            },
+            success : function(data){
+                console.log(data);
+
+            }
+        });
+
+}
+function modalCallback(e) {
+    e.preventDefault();
+    client_name = document.getElementById("modal-client-name").value;
+    client_phone = document.getElementById("modal-client-phone").value;
+    console.log('send modal form');
+        $.ajax({
+            type:"POST",
+            url:'/user/callback/',
+            data:{
+                'client_name':client_name,
+                'client_phone':client_phone
+            },
+            success : function(data){
+                console.log(data);
+
+            }
+        });
+
+}
